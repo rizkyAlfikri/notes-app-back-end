26eecb38c93d21a4d3d4fa8b95cb7bd4c6eb1266
rizkyalfikri00 <rizkyalfikri@gmail.com>
2022-07-14 21:58:41 +0700

    data validation implemented

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
 
-const init = async() => {
+const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host:  process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'],
@@ -12,7 +14,34 @@ const init = async() => {
         },
     });
 
-    server.route(routes);
+    const noteService = new NotesService();
+
+    await server.register({
+        plugin: notes,
+        options: {
+            service: noteService,
+            validator: NotesValidator,
+        },
+    });
+
+    // server.route(routes);
+
+    // registrasi single plugin
+    // await server.register({
+    //     plugin: notesPlugin,
+    //     options: { notes: [] },
+    // });
+
+    // registrasi multiple plugin
+    // await server.register([{
+    //     plugin: notesPlugin,
+    //     options: { notes: [] },
+    // },
+    // {
+    //     plugin: otherPlugin,
+    //     options: { otherOptions },
+    // },
+    // ]);
 
     await server.start();
     console.log(`Sever berjalan pada ${server.info.uri}`);
---
+++ src/validator/notes/index.js
@@ -0,0 +1,15 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const { NotePayloadSchema } = require('./schema');
+
+const NotesValidator = {
+    validateNotePayload: (payload) => {
+    const validateResult = NotePayloadSchema.validate(payload);
+
+    if (validateResult.error) {
+        throw new InvariantError(validateResult.error.message);
+    }
+    },
+};
+
+
+module.exports = NotesValidator;
